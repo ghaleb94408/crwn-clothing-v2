feedca3b18f2578c56c90c8e32a8fe43c2dad162
ghaleb94408 <ghaleb.murapyeh@gmail.com>
2023-01-06 19:54:38 +0200

    added in our sign in component as well as firebase utila and brought in firebase library

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,53 @@
+// import { useEffect } from "react";
+// import { getRedirectResult, signInWithRedirect } from "firebase/auth";
+// import {
+//   signInWithGooglePopup,
+//   signInWithGoogleRedirect,
+//   createUserDocumentFromAuth,
+//   auth,
+// } from "../../../utils/firebase/firebase.utils";
+// const SignIn = () => {
+//   useEffect(() => {
+//     const response = async () => {
+//       const res = await getRedirectResult(auth);
+//       if (res) {
+//         const userDocRef = await createUserDocumentFromAuth(res.user);
+//       }
+//       return "Done";
+//     };
+//     console.log(response().then((value) => console.log(value)));
+//   }, []);
+//   const logGoogleUser = async () => {
+//     const { user } = await signInWithGooglePopup();
+//     const userDocRef = await createUserDocumentFromAuth(user);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Sign in page</h1>
+//       <button onClick={logGoogleUser}>Sign in with google</button>
+//       <button onClick={signInWithGoogleRedirect}>
+//         Sign in with google redirect
+//       </button>
+//     </div>
+//   );
+// };
+// export default SignIn;
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils";
-const SignIn = () => {
+const signIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
+    // Add user to DB
+    const userDocRef = createUserDocumentFromAuth(user);
   };
   return (
     <div>
-      <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}>Sign in with google</button>
+      <h1>Sign in please</h1>
+      <button onClick={logGoogleUser}>Sign in bitte</button>
     </div>
   );
 };
-export default SignIn;
+export default signIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,7 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+// config from the firebase site
 const firebaseConfig = {
   apiKey: "AIzaSyAYzRCGpILN4GaKSbJeFnfKcsDSALtzaao",
   authDomain: "crwn-clothing-db-d7b29.firebaseapp.com",
@@ -15,17 +16,25 @@ const firebaseConfig = {
   appId: "1:625333851590:web:fd8ef390132e10bfe78ec7",
 };
 const firebaseApp = initializeApp(firebaseConfig);
+// To use google auth we need to set a provider
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
+  // When a user interacts with our provider we always force them to select an account.
   prompt: "select_account",
 });
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
+// Create DB
 export const db = getFirestore();
+// Take data from authentication and store it in DB
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
   const userSnapshot = await getDoc(userDocRef);
   // 1) Check if user exists
+  console.log(userSnapshot.exists());
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -36,8 +45,6 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     } catch (err) {
       console.log("Error creating user", err);
     }
-  } else {
-    console.log("not done");
   }
   return userDocRef;
 };
